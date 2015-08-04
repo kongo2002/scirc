2ae4df339468c0c3e3aa4608131e60da93cc7e3f
kongo2002 <kongo2002@googlemail.com>
2015-08-04 18:28:39 +0200

    move ChannelGatherer into separate file

---
+++ src/main/scala/com/kongo2002/scirc/ChannelGatherer.scala
@@ -0,0 +1,87 @@
+/* Copyright 2015 Gregor Uhlenheuer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kongo2002.scirc
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+
+import scala.concurrent.duration._
+
+object ChannelGatherer {
+  sealed abstract trait GathererResult
+  case class JobResult(result: String) extends GathererResult
+  case object NoResult extends GathererResult
+
+  def apply[T](channels: Iterable[ActorRef],
+      client: Client,
+      message: T,
+      finisher: (List[String], Client) => Unit,
+      timeout: FiniteDuration = 1.seconds) =
+    new ChannelGatherer[T](channels, client, message, finisher, timeout)
+}
+
+class ChannelGatherer[T](channels: Iterable[ActorRef],
+    client: Client,
+    message: T,
+    finisher: (List[String], Client) => Unit,
+    timeout: FiniteDuration = 1.seconds)
+  extends Actor with ActorLogging {
+  import ChannelGatherer._
+
+  implicit val ec = context.dispatcher
+
+  private var results = List.empty[String]
+  private var processed = 0
+
+  case object GatherTimeout
+
+  // send request to all given channels
+  val total = channels.foldLeft(0) { case (cnt, x) =>
+    x ! message
+    cnt + 1
+  }
+
+  val scheduler = context.system.scheduler
+  val gatherTimeout = scheduler.scheduleOnce(timeout, self, GatherTimeout)
+
+  private def sendResults {
+    finisher(results, client)
+    context stop self
+  }
+
+  private def check {
+    if (processed >= total) {
+      gatherTimeout.cancel
+      sendResults
+    }
+  }
+
+  def receive: Receive = {
+    case JobResult(result) =>
+      processed += 1
+      results = result :: results
+
+      check
+
+    case NoResult =>
+      processed += 1
+
+      check
+
+    case GatherTimeout =>
+      log.debug(s"ChannelGatherer timed out after $timeout")
+      sendResults
+  }
+}
